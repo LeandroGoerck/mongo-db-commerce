7d2657452497a3e9cec3384831045a085e91a829
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-06-28 01:15:48 -0300

    desafio17 - Retorne a quantidade total de produtos em uma nova coleção chamada resumoProdutos

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,11 +1,8 @@
 // Adicione o campo ultimaModificacao com a data corrente somente no sanduíche Big Mac
 db.produtos.updateOne(
   { nome: "Big Mac" },
-  { $set: { ultimaModificacao: Date() } },
+  { $currentDate: { ultimaModificacao: { $type: "timestamp" } } },
 );
 
 // Crie uma query que retorne o nome de todos os documentos em que o campo ultimaModificacao existe.
-db.produtos.find(
-  { ultimaModificacao: { $exists: true } },
-  { nome: 1, _id: 0 },
-);
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { nome: 1, _id: 0 });
